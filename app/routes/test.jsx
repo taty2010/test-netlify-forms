20ff68bced77bcc9b55353b92db6c4c5f8679285
Prince Wilson <maxcell.wilson@gmail.com>
2022-07-18 14:19:02 -0400

    Adds data attribute to test page

--- app/routes/test.jsx
+++ app/routes/test.jsx
@@ -10,7 +10,7 @@ export default function TestPage() {
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
       <h1>Welcome to Remix</h1>
-      <form method="post">
+      <form data-netlify="true" name="feedback-form" method="post">
         <input type="hidden" name="form-name" value="feedback-form" />
         <textarea name="message"  />
         <button type="submit">Submit</button>
