f8411e9d01b66bd3a2cc5cf8a625fdc70e91540e
Prince Wilson <maxcell.wilson@gmail.com>
2022-07-18 14:27:30 -0400

    Using reloadDocument

--- app/routes/test.jsx
+++ app/routes/test.jsx
@@ -1,20 +1,15 @@
 import { redirect } from "@remix-run/node";
-
-export const action = async ({request}) => {
-  const body = await request.formData();
-  console.log(body)
-  return null;
-}
+import { Form } from "@remix-run/react";
 
 export default function TestPage() {
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
       <h1>Welcome to Remix</h1>
-      <form data-netlify="true" name="feedback-form" method="post">
+      <Form reloadDocument data-netlify="true" name="feedback-form" method="post">
         <input type="hidden" name="form-name" value="feedback-form" />
         <textarea name="message"  />
         <button type="submit">Submit</button>
-      </form>
+      </Form>
     </div>
   );
 }
