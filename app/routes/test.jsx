b047c65404b5fe7d16ce44b0b9b46fabb2a08042
Prince Wilson <maxcell.wilson@gmail.com>
2022-07-18 14:32:00 -0400

    Use static form as the action route

--- app/routes/test.jsx
+++ app/routes/test.jsx
@@ -5,11 +5,11 @@ export default function TestPage() {
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
       <h1>Welcome to Remix</h1>
-      <Form reloadDocument data-netlify="true" name="feedback-form" method="post">
+      <form action="/some-form" data-netlify="true" name="feedback-form" method="post">
         <input type="hidden" name="form-name" value="feedback-form" />
         <textarea name="message"  />
         <button type="submit">Submit</button>
-      </Form>
+      </form>
     </div>
   );
 }
