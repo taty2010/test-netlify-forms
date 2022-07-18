f7170766a0d1d2909ae28dd52d342e2f57533f3c
Prince Wilson <maxcell.wilson@gmail.com>
2022-07-18 13:45:38 -0400

    test: go back to main page

--- app/routes/test.jsx
+++ app/routes/test.jsx
@@ -4,14 +4,14 @@ export const action = async ({request}) => {
   const body = await request.formData();
   console.log(body)
 
-  return redirect("/");
+  return redirect("/test");
 }
 
 export default function TestPage() {
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
       <h1>Welcome to Remix</h1>
-      <form method="post" action="/success">
+      <form method="post">
         <input type="hidden" name="form-name" value="feedback-form" />
         <textarea name="message"  />
         <button type="submit">Submit</button>
