649ee0570429e7f7e1aa89690f98a53de3ed477c
Prince Wilson <maxcell.wilson@gmail.com>
2022-07-18 13:20:01 -0400

    Adds path to Netlify form

--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -2,7 +2,7 @@ export default function Index() {
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
       <h1>Welcome to Remix</h1>
-      <form method="POST">
+      <form method="POST" action="/">
         <input type="hidden" name="form-name" value="feedback-form" />
         <textarea name="message"  />
         <button type="submit">Submit</button>
