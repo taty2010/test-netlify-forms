34e10018972b228219e50f5244e8988a622bb6a9
Prince Wilson <maxcell.wilson@gmail.com>
2022-07-18 13:17:12 -0400

    Adds method

--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -2,7 +2,7 @@ export default function Index() {
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
       <h1>Welcome to Remix</h1>
-      <form>
+      <form method="POST">
         <input type="hidden" name="form-name" value="feedback-form" />
         <textarea name="message"  />
         <button type="submit">Submit</button>
