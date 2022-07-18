13cf9b0558642d8a1d4f7fe0e8ab5ce727d449aa
Prince Wilson <maxcell.wilson@gmail.com>
2022-07-18 14:41:45 -0400

    Uses NetlifyForm component with new ergonomics

--- app/routes/test.jsx
+++ app/routes/test.jsx
@@ -1,15 +1,24 @@
 import { redirect } from "@remix-run/node";
 import { Form } from "@remix-run/react";
 
+
+const NetlifyForm = ({ action, children}) => {
+    return (
+        <form action={action} method="post">
+            <input type="hidden" name="form-name" value={action.replace("/", "")} />
+            {children}
+        </form>
+    )
+}
+
 export default function TestPage() {
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
       <h1>Welcome to Remix</h1>
-      <form action="/some-form" data-netlify="true" name="feedback-form" method="post">
-        <input type="hidden" name="form-name" value="feedback-form" />
+      <NetlifyForm action="/some-form">
         <textarea name="message"  />
         <button type="submit">Submit</button>
-      </form>
+      </NetlifyForm>
     </div>
   );
 }
