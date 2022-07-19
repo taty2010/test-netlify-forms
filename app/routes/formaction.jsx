e94fbf9ca59619e72245f7df2ca7448745d1abff
Prince Wilson <maxcell.wilson@gmail.com>
2022-07-19 08:33:03 -0400

    Add custom formaction request

---
+++ app/routes/formaction.jsx
@@ -0,0 +1,36 @@
+import { redirect } from "@remix-run/node";
+import { Form } from "@remix-run/react";
+
+export async function action({request}) {
+    console.log("Here we are making a request with the backend")
+    const data = await request.formData();
+    await fetch(`${request.url}/some-form`, {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        body: data
+    })
+
+    return redirect("/formaction")
+}
+
+
+export default function FormPage() {
+  return (
+    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
+      <h1>Welcome to Remix</h1>
+      <Form method="post">
+        <input type="hidden" name="form-name" value="some-form" />
+        {/* Replace with hidden style class */}
+        <p style={{display: "none"}}>
+            <label>
+            Don’t fill this out if you’re human: <input name="bot-field" />
+            </label>
+        </p>
+        <textarea name="message"  />
+        <button type="submit">Submit</button>
+      </Form>
+    </div>
+  );
+}
