e3266f449d184eb9c294646773962ca937f6738f
taty2010 <shydalys123@gmail.com>
2022-12-19 18:40:48 -0600

    testing out Jasons fix

--- app/routes/test.jsx
+++ app/routes/test.jsx
@@ -2,7 +2,7 @@ import { redirect } from "@remix-run/node";
 import { Form } from "@remix-run/react";
 import * as React from "react";
 
-const handleSubmit = (event) => {
+const handleSubmit = (event, htmlForm, formRedirect) => {
   event.preventDefault();
 
   const form = event.target;
@@ -10,20 +10,20 @@ const handleSubmit = (event) => {
 
   // Netlify will accept form submissions to any valid URL
   // by submitting to a static file we skip Remix's POST catcher
-  fetch('/some-form.html', {
+  fetch(htmlForm, {
     method: 'POST',
     headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
     body: new URLSearchParams(data).toString(),
   })
     .then(() => {
-      window.location.href = '/success/';
+      window.location.href = formRedirect;
     })
     .catch((error) => alert(error));
 };
 
 const NetlifyForm = ({children, formName, action}) => {
     return (
-        <form action={action} method="POST" data-netlify="false" onSubmit={handleSubmit}>
+        <form action={action} method="POST" data-netlify="false" onSubmit={ e => handleSubmit(e, '/some-form.html', '/success')}>
             <input type="hidden" name="form-name" value={formName}/>
             {/* Replace with hidden style class */}
             <p style={{display: "none"}}>
@@ -37,31 +37,13 @@ const NetlifyForm = ({children, formName, action}) => {
 }
 
 export default function TestPage() {
-
-  let [isForm, setIsForm] = React.useState(<div></div>);
-  
-  React.useEffect(() => {
-    const setForm = () => {
-      return(
+    return (
+      <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
+        <h1>Welcome to Remix</h1>
         <NetlifyForm action="/success" formName="some-form">
           <textarea name="message"  />
           <button type="submit">Submit</button>
         </NetlifyForm>
-      )
-    }
-
-    setForm();
-    setIsForm(setForm);
-  }, []);
-  // console.log(isForm)
-    return (
-      <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-        <h1>Welcome to Remix</h1>
-        {/* <NetlifyForm action="/some-form" formName="some-form">
-          <textarea name="message"  />
-          <button type="submit">Submit</button>
-        </NetlifyForm> */}
-        {isForm && isForm}
       </div>
     );
 }
